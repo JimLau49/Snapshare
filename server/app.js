2a80381f84c33c5debe1d20d03d9c2c13b01dbe1
JimLau49 <jimmy71471@gmail.com>
2023-02-09 17:04:34 -0500

    setup: project scaffolding

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const app = express();
