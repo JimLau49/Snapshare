38953eac652928a1de01e9dc86adcce09f05075c
JimLau49 <jimmy71471@gmail.com>
2023-02-11 14:15:10 -0500

    feat: add home page route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path: 'register', component:RegisterComponent},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'home', component:HomeComponent}
 ];
 
 @NgModule({
