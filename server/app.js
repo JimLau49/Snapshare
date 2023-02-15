6276191eee1fa5e1df50dcd3d1b7cb1a6adb60cb
JimLau49 <jimmy71471@gmail.com>
2023-02-14 22:34:27 -0500

    feat: host images from server

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ const userRoutes = require('./routes/user.route');
 const postRoutes = require('./routes/posts.route');
 const connectToDb = require('./configs/db.config');
 const cors = require('cors');
+const path = require('path');
 const app = express();
+const fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,9 +15,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 connectToDb();
 
 app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
+app.get('/api/images/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const imagePath = path.join(__dirname, 'uploads', filename);
+
+  res.sendFile(imagePath);
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
